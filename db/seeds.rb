4445f808be84ba7752df98c12e3fefa49da1baae
Blake Montgomery <blakemontgomery312@gmail.com>
2021-07-18 10:26:50 -0500

    Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+    Message.create(
+        name: Faker::TvShows::DrWho.character, 
+        message: Faker::TvShows::DrWho.quote, 
+        topic: "Doctor Who"
+    )
+end
+
+puts "Seeded Database"
