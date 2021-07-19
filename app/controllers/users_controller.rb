4872e2a242d25677b2cd44c8c64024d52701d3de
Chelsea L <chelsealumsden@gmail.com>
2021-07-19 16:11:55 -0400

    Users model works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
+
+  # REGISTER
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+      puts @user
+    else
+      render json: {error: "Invalid "}
+    end
+  end
+
+  # LOGGING IN
+  def login
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+      puts @user
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
+
+  def auto_login
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :password)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_190635) do
+ActiveRecord::Schema.define(version: 2021_07_19_201008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_190635) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
     t.string "password_digest"
   end
 
