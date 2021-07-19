3d1f921cf1229967260a38f5de8f1a0be46492c3
Chelsea L <chelsealumsden@gmail.com>
2021-07-19 16:37:29 -0400

    Completed User Auth

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'dotenv-rails', group: [:development, :test]
 ruby '3.0.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+HOSTNAME = ENV['YOURSECRET']
 
 module MessageAppApi
   class Application < Rails::Application
@@ -28,7 +30,6 @@ module MessageAppApi
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
