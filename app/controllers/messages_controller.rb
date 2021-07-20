bb8ad50406588a0b89b7fe61b7edde450d1dbb92
Blake Montgomery <blakemontgomery312@gmail.com>
2021-07-19 21:54:04 -0500

    Adjust route methods for Messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,41 +1,42 @@
 class MessagesController < ApplicationController
-    before_action :set_notice, only: [:show, :update, :destroy]
+    before_action :set_message, only: [:show, :update, :destroy]
+    before_action :authorized
     
     def index
         @messages = Message.all
-
         render json: @messages.to_json(include: :user)
     end
 
     def show
-        render json: @message
+        render json: @message.to_json(include: :user)
     end
 
     def create
         @message = Message.new(message_params)
+        @message.user = @user
+        
         if @message.save
-            render json: @message, status: :created, user: @message
+            render json: @message.to_json(include: :user), status: :created, user: @message
         else
             render json: @message.errors, status: :unprocessable_entity
         end
     end
 
     def update
-        message = Message.find(params[:id])
-        message.update(message_params)
-        render json: { message: message, status: 201}
-
+        if @message.update(message_params)
+            render json: @messages.to_json(include: :user)
+        else
+            render json: @message.errors, status: :unprocessable_entity
+        end
     end
 
     def destroy
-        message = Message.find(params[:id])
-        message.destroy
-        render json: { message: message, status: 204 }
+        @message.destroy
     end
 
     private
         def message_params
-            params.require(:message).permit(:message, :topic, :name)
+            params.require(:message).permit(:content, :topic, :user_id)
         end
 
         def set_message
